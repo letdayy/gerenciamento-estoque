c5d09e061e0e247b2594fe1c23cdedc1458ef8ca
letdayy <lcdayane49@gmail.com>
2023-07-05 12:55:21 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Construtor para o objeto Item
+function Item(name, quantity) {
+  this.name = name;
+  this.quantity = quantity;
+}
+
+// Função para adicionar um novo item ao estoque
+function addItem() {
+  var itemName = document.getElementById("item-name").value;
+  var itemQuantity = parseInt(document.getElementById("item-quantity").value);
+
+  // Criar uma instância do objeto Item
+  var newItem = new Item(itemName, itemQuantity);
+
+  // Adicionar o item à lista de estoque
+  var stockList = document.getElementById("stock-items");
+  var listItem = document.createElement("li");
+  listItem.textContent = newItem.name + " - Quantidade: " + newItem.quantity;
+  stockList.appendChild(listItem);
+
+  // Limpar os campos do formulário
+  document.getElementById("item-name").value = "";
+  document.getElementById("item-quantity").value = "";
+}
+
+// Evento de clique no botão "Adicionar ao Estoque"
+document.getElementById("add-button").addEventListener("click", addItem);
